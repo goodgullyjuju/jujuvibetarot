d81d165127fbfafe9bc8d6889ed0de6276ae4b9c
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-01-13 17:40:29 -0500

    Update 'OneCardDealView' for duplication of 'CardView'

--- src/OneCardDealView.js
+++ src/OneCardDealView.js
@@ -50,20 +50,8 @@ function OneCardDealView() {
             );
             
 }
-function CardView({ card, showCard, redrawCounter }) {
-    useEffect(() => {
-    setShowCard(true);
-    }, [redrawCounter]);
-    return (
-        <div>
-            <img src={`/images/${card.imageName}`} alt={card.name} className="card-animation" style={{ width: 300, height: 300, transform: `rotateY(${showCard ? '0' : '180'}deg)` }} />
-            <h2>{card.name}</h2>
-            <div style={{ maxHeight: '300px', overflowY: 'auto' }}>
-                <p>{card.interpretations}</p>
-            </div>
-        </div>
-    );
-}
+
+
 
 export default OneCardDealView;    
 
