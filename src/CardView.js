ff736d72c5b9753612eed2d6c6b670e900a48649
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-01-20 17:28:59 -0500

    reaffirm path to images in public directory

--- src/CardView.js
+++ src/CardView.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function CardView({ card, showCard, redrawCounter }) {
-    // Removed setShowCard from useEffect since it's not defined here
+    const imageUrl = `/images/${card.imageName}.png`; // Update the image URL
 
     return (
         <div>
-            <img src={`/images/${card.imageName}`} alt={card.name} className="card-animation" style={{ width: 300, height: 300, transform: `rotateY(${showCard ? '0' : '180'}deg)` }} />
+            <img src={imageUrl} alt={card.name} className="card-animation" style={{ width: 300, height: 300, transform: `rotateY(${showCard ? '0' : '180'}deg)` }} />
             <h2>{card.name}</h2>
             <div style={{ maxHeight: '300px', overflowY: 'auto' }}>
                 <p>{card.interpretations}</p>
