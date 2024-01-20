552e8919d1da4ef8307042a40b3f4a2d1ce6a12a
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-01-20 17:46:49 -0500

    Removed useCallback from drawCard function. Modified drawCard to accept an optional parameter and default to cards state. Updated useEffect to call drawCard directly with the fetched data.

--- src/OneCardDealView.js
+++ src/OneCardDealView.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import TarotButton from './TarotButton';
 import './OneCardDealView.css';
 import CardView from './CardView'; 
+
 function OneCardDealView() {
     const [cards, setCards] = useState([]);
     const [drawnCard, setDrawnCard] = useState(null);
@@ -9,26 +10,23 @@ function OneCardDealView() {
     const [redrawCounter, setRedrawCounter] = useState(0);
     const [showingSaveAlert, setShowingSaveAlert] = useState(false);
 
-    // Wrapped in useCallback to avoid useEffect dependencies issue
-    const drawCard = useCallback(() => {
-        if (cards.length === 0) return;
-        setShowCard(false);
-        const newCard = cards[Math.floor(Math.random() * cards.length)];
-        setDrawnCard(newCard);
-        setRedrawCounter(prevCounter => prevCounter + 1);
-    }, [cards]);
-
     useEffect(() => {
         fetch('/TarotCards.json')
             .then(response => response.json())
             .then(data => {
                 setCards(data);
-                if (data.length > 0) {
-                    drawCard();
-                }
+                drawCard(data);
             })
             .catch(error => console.error('Error fetching data:', error));
-    }, [drawCard]); // drawCard is now a dependency
+    }, []);
+
+    const drawCard = (tarotCards = cards) => {
+        if (tarotCards.length === 0) return;
+        setShowCard(false);
+        const newCard = tarotCards[Math.floor(Math.random() * tarotCards.length)];
+        setDrawnCard(newCard);
+        setRedrawCounter(prevCounter => prevCounter + 1);
+    };
 
     const saveEntry = () => {
         setShowingSaveAlert(true);
@@ -39,17 +37,14 @@ function OneCardDealView() {
             {drawnCard && (
                 <>
                     <CardView card={drawnCard} showCard={showCard} redrawCounter={redrawCounter} />
-                    <TarotButton title="Draw Another Card" onClick={drawCard} />
+                    <TarotButton title="Draw Another Card" onClick={() => drawCard()} />
                     <TarotButton title="Save Entry" onClick={saveEntry} />
                 </>
             )}
-            {!drawnCard && <TarotButton title="Draw a Card" onClick={drawCard} />}
+            {!drawnCard && <TarotButton title="Draw a Card" onClick={() => drawCard()} />}
             {showingSaveAlert && <div>Saved! Your card has been saved to the journal.</div>}
         </div>
     );
 }
 
-
-
 export default OneCardDealView;
-
