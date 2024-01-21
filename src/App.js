a3bdb85ae5dcdb98d669b43eb5d4ce2185765f2b
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-01-21 15:55:07 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import OneCardDealView from './OneCardDealView';
+import CardView from './CardView';
+import ContentView from './ContentView';
+import TarotCard from './TarotCard';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<OneCardDealView />} />
+        <Route path="/" element={<CardView />} />
+        <Route path="/" element={<ContentView />} />
+        <Route path="/" element={<TarotCard />} />
         {/* Add more routes here if necessary */}
       </Routes>
     </div>
