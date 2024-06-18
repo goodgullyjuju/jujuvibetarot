f1b218d1d458e8df4bb23097bb55d77bda3d97af
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-18 12:22:55 -0400

    update 'CelticCrossSpreadView.js'

--- src/CelticCrossSpreadView.js
+++ src/CelticCrossSpreadView.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import TarotButton from './TarotButton';
-
 import { saveJournalEntry } from './journalUtils.js'; // Ensure this path is correct
 
 function CelticCrossSpreadView({ goBack }) {
@@ -63,14 +62,14 @@ function CelticCrossSpreadView({ goBack }) {
                         <div key={index}>
                             <h3>{celticCrossPositions[index]}</h3> {/* Display the position label */}
                             <img
-                src={process.env.PUBLIC_URL + `/images/${card.imageName}.png`}
-                alt={card.name}
-                className="cardImage"
-                onError={(e) => { 
-                    e.target.onerror = null; 
-                    e.target.src = process.env.PUBLIC_URL + '/placeholderImage.png'; // Access placeholder from public folder
-                  }}
-              />
+                                src={`/images/${card.imageName}.png`}
+                                alt={card.name}
+                                className="cardImage"
+                                onError={(e) => { 
+                                    e.target.onerror = null; 
+                                    e.target.src = '/placeholderImage.png'; 
+                                }}
+                            />
                         </div>
                     ))}
                     <textarea
