f55051b72d8b3230369e22b5a57e68f423507197
goodgullyjuju <70729359+goodgullyjuju@users.noreply.github.com>
2024-06-18 14:09:59 -0400

    update 'TarotCard.js', 'CelticCrossSpreadView.js', and 'CardView.js'

--- src/CelticCrossSpreadView.js
+++ src/CelticCrossSpreadView.js
@@ -64,9 +64,9 @@ function CelticCrossSpreadView({ goBack }) {
                                 src={process.env.PUBLIC_URL + `/images/${card.imageName}.png`}
                                 alt={card.name}
                                 className="cardImage"
-                                onError={(e) => { 
-                                    e.target.onerror = null; 
-                                    e.target.src = process.env.PUBLIC_URL + '/placeholderImage.png'; 
+                                onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src = process.env.PUBLIC_URL + '/placeholderImage.png';
                                 }}
                             />
                         </div>
